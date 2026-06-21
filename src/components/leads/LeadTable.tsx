2dfcd166863907ba78be16460e582c7c0fc9ff84
Mohammad Faheem <webstereddy3@gmail.com>
2026-06-21 20:33:19 +0530

    fix:deployment error

--- src/components/leads/LeadTable.tsx
+++ src/components/leads/LeadTable.tsx
@@ -165,7 +165,7 @@ export default function LeadTable({ leads, onDelete, loading, onSelectionChange,
         disableRowSelectionOnClick
         onRowSelectionModelChange={(newSelectionModel) => {
           if (onSelectionChange) {
-            onSelectionChange((newSelectionModel as any[]).map(id => String(id)));
+            onSelectionChange((newSelectionModel as unknown as string[]).map(id => String(id)));
           }
         }}
         rowSelectionModel={selectedIds}
